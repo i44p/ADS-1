b6497929b0ff5a904f76e9e9190927e5b32dc6c5
i44p <jsbbdjzjsjawk+@gmail.com>
2023-02-22 12:16:15 +0300

    implement checkPrime, nPrime, nextPrime, sumPrime

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,50 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
+
 #include "alg.h"
 
+#include <cmath>
+#include <cstdint>
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 3) return true;
+
+  if (value % 2 == 0) return false;
+
+  for (uint64_t divisor = 3; divisor <= sqrt(value); divisor += 2) {
+    if (value % divisor == 0) return false;
+  }
+
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t index = 0;
+  uint64_t value = 0;
+
+  do {
+    value++;
+    if (checkPrime(value)) {
+      index++;
+    }
+  } while (index <= n);
+
+  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  do {
+    value++;
+  } while (!checkPrime(value));
+
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+
+  for (uint64_t count = 0, index = 2; count < hbound;
+       count++, index = nextPrime(index))
+    sum += index;
+
+  return sum;
 }
